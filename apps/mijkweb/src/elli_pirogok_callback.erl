043a8bedd95a573b936554381a60da287a2d4f5a
mijkenator <mijkenator@gmail.com>
2012-10-05 01:34:37 +0400

    elli test, cookie parser

--- apps/mijkweb/src/elli_pirogok_callback.erl
+++ apps/mijkweb/src/elli_pirogok_callback.erl
@@ -1,6 +1,7 @@
 -module(elli_pirogok_callback).
 -export([handle/2, handle_event/3]).
 
+-include("include/consts.hrl").
 -include_lib("elli/include/elli.hrl").
 -behaviour(elli_handler).
 
@@ -13,9 +14,81 @@ handle('GET',[<<"test">>, <<"elli">>], _Req) ->
     %% to signal success.
     {ok, [], <<"Ты кто такой, давай техзадание!">>};
 
+handle('GET',[<<"test">>, <<"elli">>, <<"cookie">>], Req) ->
+    case elli_request:get_header(<<"Cookie">>, Req, undefined) of
+        undefined -> lager:debug("E:NC", []), {ok, mcache_get_session_header(undefined), <<"No cookie \n">>};
+        Cookies   -> 
+            lager:debug("E:C: ~p ~n", [Cookies]),
+            {RespHeaders, RespBody} = case [{Key, Value} || {Key, Value} <- 
+                                            tests:lt_parse_cookie(Cookies), Key =:= <<"MIJKSSID">> ] of
+                []          ->
+                    {mcache_get_session_header(undefined), list_to_binary("Cookie:" ++ io_lib:format("~p",[Cookies]))};
+                [{_, SSID}] ->
+                    lager:debug("E:C:SSID ~p",[SSID]),
+                    IpS = list_to_binary(io_lib:format("~p", [inet:getifaddrs()])),
+                    {mcache_get_session_header(SSID), <<"Kolobok alive!\n<br>", IpS/binary>>}
+            end,
+            {ok, RespHeaders, RespBody}
+    end;
+
+handle('GET',[<<"test">>, <<"elli">>, <<"cookie">>, <<"mcd">>], Req) ->
+    case elli_request:get_header(<<"Cookie">>, Req, undefined) of
+        undefined -> lager:debug("E:NC", []), {ok, mcd_get_session_header(undefined), <<"No cookie \n">>};
+        Cookies   -> 
+            lager:debug("E:C: ~p ~n", [Cookies]),
+            {RespHeaders, RespBody} = case [{Key, Value} || {Key, Value} <- 
+                                            tests:lt_parse_cookie(Cookies), Key =:= <<"MIJKSSID">> ] of
+                []          ->
+                    {mcd_get_session_header(undefined), list_to_binary("Cookie:" ++ io_lib:format("~p",[Cookies]))};
+                [{_, SSID}] ->
+                    lager:debug("E:C:SSID ~p",[SSID]),
+                    IpS = list_to_binary(io_lib:format("~p", [inet:getifaddrs()])),
+                    {mcd_get_session_header(SSID), <<"Kolobok alive!\n<br>", IpS/binary>>}
+            end,
+            {ok, RespHeaders, RespBody}
+    end;
+
 handle(_, _, _Req) ->
     {404, [], <<"Ты кто такой, давай досвидания!">>}.
 
 %% @doc: Handle request events, like request completed, exception
 %% thrown, client timeout, etc. Must return 'ok'.
 handle_event(_Event, _Data, _Args) -> ok.
+
+mcache_get_session_header(SessionCookie) ->
+    case SessionCookie of
+        undefined ->
+            [cowboy_cookies:cookie(<<"MIJKSSID">>, mijk_session:mcache_create_session(1),[{max_age, ?SESSION_AGE}])];
+        SSID      ->
+            case mijk_session:mcache_check_session_data(SSID) of
+                {ok, Data} when is_binary(Data) ->
+                    EData = mijk_session:mysql_to_erl(Data),
+                    NewData = case proplists:get_value(<<"counter">>, EData, undefined) of
+                        undefined            -> [{<<"counter">>, 1}] ++ EData;
+                        V when is_integer(V) -> lists:keyreplace(<<"counter">>, 1, EData, {<<"counter">>, V+1})
+                    end,
+                    mijk_session:mcache_update_session(SSID, NewData),
+                    [cowboy_cookies:cookie(<<"MIJKSSID">>, SSID,[{max_age, ?SESSION_AGE}])]
+                ;_ ->
+                    [cowboy_cookies:cookie(<<"MIJKSSID">>, mijk_session:mcache_create_session(1),[{max_age, ?SESSION_AGE}])]
+            end
+    end.
+
+mcd_get_session_header(SessionCookie) ->
+    case SessionCookie of
+        undefined ->
+            [cowboy_cookies:cookie(<<"MIJKSSID">>, mijk_session:mcd_create_session(1),[{max_age, ?SESSION_AGE}])];
+        SSID      ->
+            case mijk_session:mcd_check_session_data(SSID) of
+                {ok, Data} when is_binary(Data) ->
+                    EData = mijk_session:mysql_to_erl(Data),
+                    NewData = case proplists:get_value(<<"counter">>, EData, undefined) of
+                        undefined            -> [{<<"counter">>, 1}] ++ EData;
+                        V when is_integer(V) -> lists:keyreplace(<<"counter">>, 1, EData, {<<"counter">>, V+1})
+                    end,
+                    mijk_session:mcd_update_session(SSID, NewData),
+                    [cowboy_cookies:cookie(<<"MIJKSSID">>, SSID,[{max_age, ?SESSION_AGE}])]
+                ;_ ->
+                    [cowboy_cookies:cookie(<<"MIJKSSID">>, mijk_session:mcd_create_session(1),[{max_age, ?SESSION_AGE}])]
+            end
+    end.
--- apps/mijkweb/src/tests.erl
+++ apps/mijkweb/src/tests.erl
@@ -11,7 +11,14 @@
     get_inst1/0,
     get_inst2/0,
     get_inst1_i/1,
-    get_inst2_i/1
+    get_inst2_i/1,
+
+    parse_cookie_cw/0,
+    parse_cookie_cw_i/1,
+    lt_parse_cookie/1,
+    parse_cookie_lt/0,
+    parse_cookie_lt_i/1,
+    reverse/1
 ]).
 
 
@@ -55,4 +62,74 @@ get_inst1_i(N) -> mijk_session_manager:get_instance(mijk_session:generate_sessio
 
 get_inst2_i(0) -> ok;
 get_inst2_i(N) -> mijk_session_manager:get_instance(mijk_session:generate_session_uuid(1), 5), get_inst2_i(N-1).
+
+parse_cookie_cw() -> test("cookie parse cw", fun() -> tests:parse_cookie_cw_i(1000000) end).
+
+parse_cookie_cw_i(0) -> ok;
+parse_cookie_cw_i(N) ->
+	C1 = <<"$Version=\"1\"; Customer=\"WILE_E_COYOTE\"; $Path=\"/acme\";
+	Part_Number=\"Rocket_Launcher_0001\"; $Path=\"/acme\";
+	Shipping=\"FedEx\"; $Path=\"/acme\"">>,
+    cowboy_cookies:parse_cookie(C1)
+    , parse_cookie_cw_i(N - 1).
+    
+
+parse_cookie_lt() -> test("cookie parse lt", fun() -> tests:parse_cookie_lt_i(1000000) end).
+
+parse_cookie_lt_i(0) -> ok;
+parse_cookie_lt_i(N) ->
+	C1 = <<"$Version=\"1\"; Customer=\"WILE_E_COYOTE\"; $Path=\"/acme\";
+	Part_Number=\"Rocket_Launcher_0001\"; $Path=\"/acme\";
+	Shipping=\"FedEx\"; $Path=\"/acme\"">>,
+    %io:format("PCLI-> ~p ~n", [N]),
+    lt_parse_cookie(C1)
+    , parse_cookie_lt_i(N - 1).
+
+lt_parse_cookie(String) ->
+    lists:flatten([lt_parse_cookie_i(X) || X <- binary:split(String, <<";">>, [global]), X=/=<<>>]).
+
+lt_parse_cookie_i(<<$$,_/binary>>) -> [];
+lt_parse_cookie_i(S) ->
+    case binary:split(S, <<"=">>) of
+        [K, V]    -> {lt_c_cl(K), lt_c_cl(V)};
+        [K, <<>>] -> {lt_c_cl(K), []};
+        [K]       -> {lt_c_cl(K), []}
+    end.
+
+lt_c_cl(String) ->
+    S1 = skip_whitespace(String),
+    reverse(skip_whitespace(reverse(S1))).
+
+skip_whitespace(String) -> binary_dropwhile(fun is_whitespace/1, String).
+
+binary_dropwhile(_, <<"">>) -> <<"">>;
+binary_dropwhile(F, <<C, Rest/binary>> = String) ->
+    case F(C) of
+	    true  -> binary_dropwhile(F, Rest);
+	    false -> String
+    end.
+
+is_whitespace($\s) -> true;
+is_whitespace($\t) -> true;
+is_whitespace($\r) -> true;
+is_whitespace($\n) -> true;
+is_whitespace($")  -> true;
+is_whitespace(_)   -> false.
+
+reverse(Binary) -> 
+    list_to_binary(lists:reverse(binary_to_list(Binary))).
+%reverse(Binary) ->
+%    Size = size(Binary)*8,
+%    <<T:Size/integer-little>> = Binary,
+%    <<T:Size/integer-big>>.
+
+%lt_c_cl(String) when size(String) > 3 ->
+%    {match, [[Trimmed]]} = re2:match(String, "^\\s*\"*([^\\s\"]+.+[^\\s\"]+)\"*\\s*$", 
+%        [{capture, all_but_first, binary}, global, dollar_endonly, unicode, dotall]), Trimmed;
+%lt_c_cl(String) when size(String) > 3 ->
+%    {match, [Trimmed]} = re2:match(String, "^\\s*\"*([^\\s\"]+.+[^\\s\"]+)\"*\\s*$", 
+%        [{capture, all_but_first, binary}]), Trimmed;
+%lt_c_cl(<<$",R/utf8,$">>) -> <<R>>;
+%lt_c_cl(String)           -> re2:replace(String, ["\\s"], [], [global, {return, binary}]).
+%lt_c_cl(String)           -> re2:replace(String, ["\\s"], [], []).
 
